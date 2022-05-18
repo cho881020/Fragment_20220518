4ff7ac73732b6775d52d75316a4e6b255bbfa7e4
조경진 <cho881020@gmail.com>
2022-05-18 13:07:04 +0900

    SecondFragment 추가 => 복사 / 붙여넣기 활용

---
+++ app/src/main/java/kr/nepp/fragment_20220518/fragments/SecondFragment.kt
@@ -0,0 +1,33 @@
+package kr.nepp.fragment_20220518.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
+import kr.nepp.fragment_20220518.R
+
+class SecondFragment : Fragment() {
+
+//    어떤 xml을 끌어다 사용할지 결정하는 함수
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate( R.layout.fragment_second, container, false )
+    }
+
+//    동작 관련 코드 전담 함수
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+
+
+}
